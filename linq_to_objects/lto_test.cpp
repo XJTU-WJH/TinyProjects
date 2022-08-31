245dee7309a067c64e30b9bf1713ed11fb13f5b5
wjh <393814041@qq.com>
2022-08-31 10:51:00 +0800

    update lto_test

---
+++ linq_to_objects/lto_test.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+
+#include"linq_to_objects.hpp"
+
+struct Person
+{
+	int age;
+	string name;
+	string address;
+};
+
+template<typename T>
+class A
+{
+public:
+	A(T value):val(value){}
+	
+	void test()
+	{
+		cout<<val<<endl;	
+	}
+private:
+	T val;
+};
+
+void TestLinqCpp()
+{
+	vector<Person>v={{21,"wjh","shanghai"},{22,"bb","wuhan"},{20,"bqf","zhuhai"}};
+	auto count=from(v).where([](const Person& p){return p.age>20;}).count();
+	cout<<count<<endl;
+}
+
+int main()
+{
+	TestLinqCpp();
+    return 0;
+}
